ccb3cb7442657bf4f002d6dc8a709cdda25da763
ryan <ryancruse1807@gmail.com>
2022-05-08 17:15:29 +0100

    Project Finished

--- Resources/JavaScript/main.js
+++ Resources/JavaScript/main.js
@@ -1,4 +1,10 @@
+const messageHolder = document.getElementById('message__holder');
+
+
+
+
 const generateMessage = () =>{
+    
     const greetings = ['Hello', 'Hey', 'Hi', 'Sup', 'Howdy', 'Greetings'];
     const colors = ['Orange', 'Red', 'Green', 'Blue', 'Purple', 'Yellow'];
     const hobbies = [
@@ -13,12 +19,9 @@ const generateMessage = () =>{
     const getRandomGreeting = Math.floor(Math.random() * greetings.length);
     const getRandomColor = Math.floor(Math.random() * colors.length);
     const getRandomHobby = Math.floor(Math.random() * hobbies.length);
-    
-    return `${greetings[getRandomGreeting]}, let me tell you about some random facts about me. My favorite color is ${colors[getRandomColor]} and one of my many hobbies is ${hobbies[getRandomHobby]}. `
+    messageHolder.innerHTML = `${greetings[getRandomGreeting]}, let me tell you about some random facts about me. My favorite color is ${colors[getRandomColor]} and one of my many hobbies is ${hobbies[getRandomHobby]}. `;
+    return messageHolder;
     
 };
 
 
-console.log(generateMessage());
-console.log(generateMessage());
-console.log(generateMessage());
